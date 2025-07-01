488e6c8863a69e3ba0a75a153e2cb6d6ff444ad3
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-07-01 15:32:10 +0100

    E2E simple auth solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,38 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  beforeEach(() => {
+  beforeEach(() => {});
 
+  it('should login with valid credentials', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.contains('Login Page');
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa-sign-in').click();
+    cy.url().should('include', '/secure');
+    cy.contains('Welcome to the Secure Area.');
   });
 
-  it('', () => {
+  it('should not login with invalid credentials', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.contains('Login Page');
+    cy.get('#username').type('invalidUser');
+    cy.get('#password').type('invalidPassword');
+    cy.get('.fa-sign-in').click();
+    cy.url().should('include', '/login');
+    cy.contains('Your username is invalid!');
+  });
 
+  it('should logout successfully', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.contains('Login Page');
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa-sign-in').click();
+    cy.url().should('include', '/secure');
+    cy.contains('Welcome to the Secure Area.');
+    cy.get('a.button').click();
+    cy.url().should('include', '/login');
+    cy.contains('Login Page');
   });
 });
